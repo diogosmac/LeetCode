988a4f3606831782c3894b7b04e092a4b62197f2
Diogo Machado <diogo.smac@gmail.com>
2023-05-13 17:31:57 +0100

    Problem 1: Two Sum (improved)

--- python/1-two-sum.py
+++ python/1-two-sum.py
@@ -1,10 +1,9 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        i = len(nums) - 1
-        while i > 0:
-            j = i - 1
-            while j >= 0:
-                if nums[i] + nums[j] == target:
-                    return [j, i]
-                j -= 1
-            i -= 1
+        visited = {}
+        for i, n in enumerate(nums):
+            diff = target - n
+            if diff in visited:
+                return [visited[diff], i]
+            visited[n] = i
+        return None
